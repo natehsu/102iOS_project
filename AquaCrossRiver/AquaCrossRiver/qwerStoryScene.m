6a414876ce7885d84de089baba5750b485863dd0
frankiejou <frankiejou@gmail.com>
2014-01-15 22:49:21 +0800

    new

--- AquaCrossRiver/AquaCrossRiver/qwerStoryScene.m
+++ AquaCrossRiver/AquaCrossRiver/qwerStoryScene.m
@@ -33,7 +33,6 @@
          [SKAction sequence:@[
                               [SKAction waitForDuration:10],
                               [SKAction runBlock:^{
-             // 5演示了在Sprite Kit中如何过渡到新的场景。首先可以选择任意的一种不同的动画过渡效果，用于场景的显示，在这里选择了翻转效果(持续0.5秒)。然后是创建一个想要显示的场景，接着使用self.view的方法presentScene:transition:来显示出场景。
              SKTransition *reveal = [SKTransition flipHorizontalWithDuration:0.5];
              SKScene * myScene = [[qwerMyScene alloc] initWithSize:self.size];
              [self.view presentScene:myScene transition: reveal];
